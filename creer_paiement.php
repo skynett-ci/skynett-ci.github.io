9b377e783ab2b03a7199bd18d873b869dbd3b629
skynett-ci <101103264+skynett-ci@users.noreply.github.com>
2026-07-12 15:03:39 +0000

    Create JSON response for payment information
    Add JSON response for payment data

--- creer_paiement.php
+++ creer_paiement.php
@@ -1,1 +1,13 @@
+<?php
+// On indique au serveur que l'on envoie du JSON
+header('Content-Type: application/json');
 
+// Vos données de paiement
+$data = [
+    "id" => "test_123",
+    "checkout_url" => "https://pay.wave.com/m/M_ci_dbe-JFInO8Of/c/ci/"
+];
+
+// On affiche le JSON proprement
+echo json_encode($data);
+?>
